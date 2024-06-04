74ec3724f6ce758702416f5f3707f1e1cc990d04
Steve Ngalamo <egoue2001@gmail.com>
2024-06-04 12:57:58 +0200

    fix-up: fix warning about present of zone by using zoneless change detection

--- projects/beuth-verlag-app/src/app/app.config.ts
+++ projects/beuth-verlag-app/src/app/app.config.ts
@@ -1,8 +1,8 @@
-import {ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection} from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideExperimentalZonelessChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {routes} from './app.routes';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
