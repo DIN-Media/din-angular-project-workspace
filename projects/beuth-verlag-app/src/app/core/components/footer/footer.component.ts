13acd994b28ad812d87083784778797793adc06b
Steve Ngalamo <egoue2001@gmail.com>
2024-06-12 21:22:42 +0200

    feat: hyperlink for newsletter added

--- projects/beuth-verlag-app/src/app/core/components/footer/footer.component.ts
+++ projects/beuth-verlag-app/src/app/core/components/footer/footer.component.ts
@@ -5,6 +5,7 @@ import socialMediasDataset from "../../../../assets/data/social-medias-links.jso
 import {Hyperlink} from "../../../models/hyperlink";
 import {NgOptimizedImage} from "@angular/common";
 import {MatButtonModule} from "@angular/material/button";
+import {HyperlinksNames} from "../../const";
 
 @Component({
   selector: 'app-footer',
@@ -16,7 +17,12 @@ import {MatButtonModule} from "@angular/material/button";
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  readonly HyperlinksNames = HyperlinksNames;
   readonly hyperlinks: Hyperlink[] = hyperlinksDataset as Hyperlink[];
   readonly socialMedias: Hyperlink[] = socialMediasDataset as Hyperlink[];
-  readonly resetPasswordText: string = "Reset Password";
+  readonly excludedLinks: string[] = [this.HyperlinksNames.NEWSLETTER, this.HyperlinksNames.RESET_PASSWORT];
+
+  get getNewsletterLink(): Hyperlink {
+    return this.hyperlinks.find((hyperlink: Hyperlink): boolean => hyperlink.name === this.HyperlinksNames.NEWSLETTER) as Hyperlink;
+  }
 }
--- projects/beuth-verlag-app/src/app/core/const/index.ts
+++ projects/beuth-verlag-app/src/app/core/const/index.ts
@@ -1,3 +1,6 @@
+import hyperlinksDataset from "../../../assets/data/hyperlinks.json";
+import {Hyperlink} from "../../models/hyperlink";
+
 export class RoutingPaths {
   static readonly HOME: string = 'home';
   static readonly AUTHENTICATE: string = 'authenticate';
@@ -21,4 +24,11 @@ export class ErrorMessage {
 
   // Password
   static readonly PASSWORD_REQUIRED: string = 'Passwort erforderlich';
+}
+
+export class HyperlinksNames {
+  private static readonly hyperlinks: Hyperlink[] = hyperlinksDataset as Hyperlink[];
+
+  static readonly NEWSLETTER: string = this.hyperlinks[0].name;
+  static readonly RESET_PASSWORT: string = this.hyperlinks[1].name;
 }
